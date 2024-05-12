d550baf3e2900bdefc65d61f7a39cea8259dc6a0
paul-mcn <paul.a.mcnamara@outlook.com>
2024-05-13 00:24:30 +1000

    undid aws .env mv

--- src/dependencies/aws.py
+++ src/dependencies/aws.py
@@ -18,16 +18,10 @@ cors_configuration = {
 
 dynamodb_client = boto3.client(
     "dynamodb",
-    aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-    region_name=settings.AWS_REGION_NAME,
 )
 
 s3_client = boto3.client(
     "s3",
-    aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-    region_name=settings.AWS_REGION_NAME,
 )
 
 # s3_client.put_bucket_cors(
